b716c9a1ba3f21c62926be69030a6d9173d0857c
Martin Fenner <martin@front-matter.io>
2022-09-18 12:51:51 +0200

    Update doi_utils.rb

--- lib/briard/doi_utils.rb
+++ lib/briard/doi_utils.rb
@@ -41,7 +41,7 @@ module Briard
     end
 
     def doi_from_url(url)
-      if /\A(?:(http|https):\/\/(dx\.)?(doi.org|handle.stage.datacite.org|handle.test.datacite.org)\/)?(doi:)?(10\.\d{4,5}\/.+)\z/.match(url)
+      if /\A(?:(http|https):\/\/(dx\.)?(doi\.org|handle\.stage\.datacite\.org|handle\.test\.datacite\.org)\/)?(doi:)?(10\.\d{4,5}\/.+)\z/.match(url)
         uri = Addressable::URI.parse(url)
         uri.path.gsub(/^\//, '').downcase
       end
