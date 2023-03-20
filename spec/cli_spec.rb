3b7482acfe62fd1735e262ed7c5d5b8828504b82
Martin Fenner <martin@front-matter.io>
2023-03-20 13:21:12 +0100

    Update cli_spec.rb

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -8,136 +8,136 @@ describe Commonmeta::CLI do
     described_class.new
   end
 
-  describe 'convert from id', vcr: true do
-    SCHEMA_ORG_REGEX = %r{"@context": "http://schema.org"}
+  describe "convert from id", vcr: true do
+    SCHEMA_ORG_REGEX = /"@context": "http:\/\/schema.org"/
     CROSSREF_XML_REGEX = /<doi_batch/
     DATACITE_REGEX = /"types": {/
-    BIBTEX_REGEX = %r{@article{https://doi.org/10.7554/elife.01567}
-    BIBTEX_DATA_REGEX = %r{@misc{https://doi.org/10.5061/dryad.8515}
+    BIBTEX_REGEX = /@article{https:\/\/doi.org\/10.7554\/elife.01567/
+    BIBTEX_DATA_REGEX = /@misc{https:\/\/doi.org\/10.5061\/dryad.8515/
     RIS_REGEX = /TY  - JOUR/
     CSL_REGEX = 'type: "article-journal'
     CITATION_REGEX = /Sankar M, Nieminen K, Ragni L, Xenarios I/
     CITATION_DATA_REGEX = /Ollomo B, Durand P, Prugnolle F/
     JATS_REGEX = /article-title/
 
-    context 'crossref' do
-      let(:input) { '10.7554/eLife.01567' }
+    context "crossref" do
+      let(:input) { "10.7554/eLife.01567" }
 
       it 'default' do
         expect { subject.convert input }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { from: 'crossref', to: 'schema_org' }
+        subject.options = { from: "crossref", to: "schema_org" }
         expect { subject.convert input }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to crossref_xml' do
-        subject.options = { from: 'crossref', to: 'crossref_xml' }
+        subject.options = { from: "crossref", to: "crossref_xml" }
         expect { subject.convert input }.to output(CROSSREF_XML_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { from: 'crossref', to: 'datacite' }
+        subject.options = { from: "crossref", to: "datacite" }
         expect { subject.convert input }.to output(DATACITE_REGEX).to_stdout
       end
 
       it 'to bibtex' do
-        subject.options = { from: 'crossref', to: 'bibtex' }
+        subject.options = { from: "crossref", to: "bibtex" }
         expect { subject.convert input }.to output(BIBTEX_REGEX).to_stdout
       end
 
       it 'to citation' do
-        subject.options = { from: 'crossref', to: 'citation', style: 'vancouver' }
+        subject.options = { from: "crossref", to: "citation", style: "vancouver" }
         expect { subject.convert input }.to output(CITATION_REGEX).to_stdout
       end
     end
 
-    context 'crossref' do
-      let(:input) { '10.7554/eLife.01567' }
+    context "crossref" do
+      let(:input) { "10.7554/eLife.01567" }
 
       it 'default' do
         expect { subject.convert input }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { to: 'schema_org' }
+        subject.options = { to: "schema_org" }
         expect { subject.convert input }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to crossref_xml' do
-        subject.options = { to: 'crossref_xml' }
+        subject.options = { to: "crossref_xml" }
         expect { subject.convert input }.to output(CROSSREF_XML_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { to: 'datacite' }
+        subject.options = { to: "datacite" }
         expect { subject.convert input }.to output(DATACITE_REGEX).to_stdout
       end
 
       it 'to bibtex' do
-        subject.options = { to: 'bibtex' }
+        subject.options = { to: "bibtex" }
         expect { subject.convert input }.to output(BIBTEX_REGEX).to_stdout
       end
 
       it 'to citation' do
-        subject.options = { to: 'citation', style: 'vancouver' }
+        subject.options = { to: "citation", style: "vancouver" }
         expect { subject.convert input }.to output(CITATION_REGEX).to_stdout
       end
 
       it 'to jats' do
-        subject.options = { to: 'jats' }
+        subject.options = { to: "jats" }
         expect { subject.convert input }.to output(JATS_REGEX).to_stdout
       end
     end
 
-    context 'datacite' do
-      let(:input) { '10.5061/dryad.8515' }
+    context "datacite" do
+      let(:input) { "10.5061/dryad.8515" }
 
       it 'default' do
         expect { subject.convert input }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { to: 'schema_org' }
+        subject.options = { to: "schema_org" }
         expect { subject.convert input }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to bibtex' do
-        subject.options = { to: 'bibtex' }
+        subject.options = { to: "bibtex" }
         expect { subject.convert input }.to output(BIBTEX_DATA_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { to: 'datacite' }
+        subject.options = { to: "datacite" }
         expect { subject.convert input }.to output(DATACITE_REGEX).to_stdout
       end
 
       it 'to citation' do
-        subject.options = { to: 'citation', style: 'vancouver' }
+        subject.options = { to: "citation", style: "vancouver" }
         expect { subject.convert input }.to output(CITATION_DATA_REGEX).to_stdout
       end
 
       it 'to jats' do
-        subject.options = { to: 'jats' }
+        subject.options = { to: "jats" }
         expect { subject.convert input }.to output(/data-title/).to_stdout
       end
     end
 
-    context 'schema_org' do
-      let(:id) { 'https://blog.datacite.org/eating-your-own-dog-food' }
+    context "schema_org" do
+      let(:id) { "https://blog.datacite.org/eating-your-own-dog-food" }
 
       it 'default' do
         expect { subject.convert id }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { to: 'schema_org' }
+        subject.options = { to: "schema_org" }
         expect { subject.convert id }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { to: 'datacite' }
+        subject.options = { to: "datacite" }
         expect { subject.convert id }.to output(DATACITE_REGEX).to_stdout
       end
 
@@ -149,137 +149,136 @@ describe Commonmeta::CLI do
     end
   end
 
-  describe 'convert file' do
-    context 'bibtex' do
-      let(:file) { fixture_path + 'crossref.bib' }
+  describe "convert file" do
+    context "bibtex" do
+      let(:file) { fixture_path + "crossref.bib" }
 
       it 'default' do
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { to: 'schema_org' }
+        subject.options = { to: "schema_org" }
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { to: 'datacite' }
+        subject.options = { to: "datacite" }
         expect { subject.convert file }.to output(DATACITE_REGEX).to_stdout
       end
     end
 
-    context 'crossref_xml', vcr: true do
-      let(:file) { fixture_path + 'crossref.xml' }
+    context "crossref_xml", vcr: true do
+      let(:file) { fixture_path + "crossref.xml" }
 
       it 'default' do
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { to: 'schema_org' }
+        subject.options = { to: "schema_org" }
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to crossref_xml' do
-        subject.options = { to: 'crossref_xml' }
+        subject.options = { to: "crossref_xml" }
         expect { subject.convert file }.to output(CROSSREF_XML_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { to: 'datacite' }
+        subject.options = { to: "datacite" }
         expect { subject.convert file }.to output(DATACITE_REGEX).to_stdout
       end
 
       it 'to bibtex' do
-        subject.options = { to: 'bibtex' }
+        subject.options = { to: "bibtex" }
         expect { subject.convert file }.to output(BIBTEX_REGEX).to_stdout
       end
     end
 
-    context 'crossref', vcr: true do
-      let(:file) { fixture_path + 'crossref.json' }
+    context "crossref", vcr: true do
+      let(:file) { fixture_path + "crossref.json" }
 
       it 'default' do
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { to: 'schema_org' }
+        subject.options = { to: "schema_org" }
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to crossref_xml' do
-        subject.options = { to: 'crossref_xml' }
+        subject.options = { to: "crossref_xml" }
         expect { subject.convert file }.to output(CROSSREF_XML_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { to: 'datacite' }
+        subject.options = { to: "datacite" }
         expect { subject.convert file }.to output(DATACITE_REGEX).to_stdout
       end
 
       it 'to bibtex' do
-        subject.options = { to: 'bibtex' }
+        subject.options = { to: "bibtex" }
         expect { subject.convert file }.to output(BIBTEX_REGEX).to_stdout
       end
     end
 
-    context 'datacite', vcr: true do
-      let(:file) { fixture_path + 'datacite.json' }
+    context "datacite", vcr: true do
+      let(:file) { fixture_path + "datacite.json" }
 
       it 'default' do
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { to: 'schema_org' }
+        subject.options = { to: "schema_org" }
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { to: 'datacite' }
+        subject.options = { to: "datacite" }
         expect { subject.convert file }.to output(DATACITE_REGEX).to_stdout
       end
 
       it 'to bibtex' do
-        subject.options = { to: 'bibtex' }
-        expect { subject.convert file }.to output(%r{@article{https://doi.org/10.5438/4k3m-nyvg}).to_stdout
+        subject.options = { to: "bibtex" }
+        expect { subject.convert file }.to output(/@article{https:\/\/doi.org\/10.5438\/4k3m-nyvg/).to_stdout
       end
 
       it 'to datacite invalid' do
-        file = fixture_path + 'datacite_missing_creator.xml'
-        subject.options = { to: 'datacite', show_errors: 'true' }
-        expect {
- subject.convert file }.to output(/root is missing required keys: id, type, url, creators, titles, publisher, date/).to_stderr
+        file = fixture_path + "datacite_missing_creator.xml"
+        subject.options = { to: "datacite", show_errors: "true" }
+        expect { subject.convert file }.to output(/root is missing required keys: id, type, url, creators, titles, publisher, date/).to_stderr
       end
 
       it 'to datacite invalid ignore errors' do
-        file = fixture_path + 'datacite_missing_creator.xml'
-        subject.options = { to: 'datacite' }
+        file = fixture_path + "datacite_missing_creator.xml"
+        subject.options = { to: "datacite" }
         expect { subject.convert file }.to output(DATACITE_REGEX).to_stdout
       end
     end
 
-    context 'codemeta' do
-      let(:file) { fixture_path + 'codemeta.json' }
+    context "codemeta" do
+      let(:file) { fixture_path + "codemeta.json" }
 
       it 'default' do
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to schema_org' do
-        subject.options = { to: 'schema_org' }
+        subject.options = { to: "schema_org" }
         expect { subject.convert file }.to output(SCHEMA_ORG_REGEX).to_stdout
       end
 
       it 'to datacite' do
-        subject.options = { to: 'datacite' }
+        subject.options = { to: "datacite" }
         expect { subject.convert file }.to output(DATACITE_REGEX).to_stdout
       end
 
       it 'to bibtex' do
-        subject.options = { to: 'bibtex' }
-        expect { subject.convert file }.to output(%r{@misc{https://doi.org/10.5063/f1m61h5x}).to_stdout
+        subject.options = { to: "bibtex" }
+        expect { subject.convert file }.to output(/@misc{https:\/\/doi.org\/10.5063\/f1m61h5x/).to_stdout
       end
     end
 
@@ -292,18 +291,18 @@ describe Commonmeta::CLI do
     # end
   end
 
-  describe 'encode' do
-    let(:input) { '10.53731' }
+  describe "encode" do
+    let(:input) { "10.53731" }
 
-    it 'blog prefix' do
-      expect { subject.encode input }.to output(%r{https://doi.org/10.53731}).to_stdout
+    it "blog prefix" do
+      expect { subject.encode input }.to output(/https:\/\/doi.org\/10.53731/).to_stdout
     end
   end
 
-  describe 'decode' do
-    let(:input) { '10.53731/cjx855h-hn5jtq8' }
+  describe "decode" do
+    let(:input) { "10.53731/cjx855h-hn5jtq8" }
 
-    it 'blog post' do
+    it "blog post" do
       expect { subject.decode input }.to output(/464528469187255429864\n/).to_stdout
     end
   end
